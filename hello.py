73b077ef329a4c683da0699ea53f59f45f74e525
wtang2019 <46497369+wtang2019@users.noreply.github.com>
2022-05-23 10:14:51 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+import newrelic.agent
+newrelic.agent.initialize('/Users/wtang/VSCodeProjects/python-flask/newrelic.ini')
+
+from flask import Flask
+from flask import Flask, render_template, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/messages/<int:idx>')
+def message(idx):
+    messages = ['Message Zero', 'Message One', 'Message Two']
+    try:
+        return render_template('message.html', message=messages[idx])
+    except IndexError:
+        abort(500)
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+@app.route('/500')
+def error500():
+    abort(500)
